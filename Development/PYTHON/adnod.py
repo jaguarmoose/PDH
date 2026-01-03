d5a31424ed10526ce78b4fa03683f0bc6d48ee71
Robert Farnan <racfarnan@gmail.com>
2026-01-03 14:24:40 -0500

    fix escape messages

--- Development/PYTHON/adnod.py
+++ Development/PYTHON/adnod.py
@@ -1,13 +1,13 @@
+import os
+
 def adnod(ns):      # Add a kid node -given parent node - this creates system folder
-    import os
     path = ns2path(ns)
     os.mkdir(path)  # need to do a whole bunch of error checking
-    path=path+"\s.0"
-    fs0=open(path,"w")
-    fs0.close
+    path = os.path.join(path, "s.0")
+    with open(path, "w"):
+        pass
 # Add a kid node -given parent node - this creates system folder
 def nxtkid(pns):
-    import os
     i=1
     kns=pns+":"+str(i)
     path = ns2path(kns)
@@ -19,18 +19,18 @@ def nxtkid(pns):
     return kns
 
 def ns2path(ns):    # convert a nodestring to dir path
-    path=r'C:\PDH\DATA'
+    path = r'C:\PDH\DATA'
     nanc= ns.split(":")
     nlev = len(nanc)
     for I in range(1,nlev):
-        path = path + "\L" + str(I)+ "K"+ nanc[I]
+        path = os.path.join(path, f"L{I}K{nanc[I]}")
     print(path)
     return path
 def uns2path(uns):    # convert a user number to dir path
-    urpath= r'C:\PDH\USER'
+    urpath = r'C:\PDH\USER'
     nanc= uns.split(":")
     nlev = len(nanc)
     for I in range(1,nlev):
-        urpath = urpath + "\L" + str(I)+ "K"+ nanc[I]
+        urpath = os.path.join(urpath, f"L{I}K{nanc[I]}")
     print(urpath)
     return urpath
